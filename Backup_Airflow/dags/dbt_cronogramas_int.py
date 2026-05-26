f37a2f834a7edad7871aa6f467c48a6f288f5810
WollaceDantas <wollacecavalcante@outlook.com.br>
2026-05-26 16:24:57 -0300

    criação da dag e modelos dbt para união de colunas duplicadas e gerar schema 'int_cronogramas'

---
+++ Backup_Airflow/dags/dbt_cronogramas_int.py
@@ -0,0 +1,62 @@
+import os
+from datetime import datetime
+from airflow import DAG
+from airflow.sdk import Param
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig, RenderConfig
+from cosmos.profiles import PostgresUserPasswordProfileMapping
+
+# --- CONFIGURAÇÕES BÁSICAS ---
+BASE_DIR = "/opt/airflow"
+DBT_PROJECT_PATH = os.path.join(BASE_DIR, "dags/dbt/cronogramas")
+DBT_EXECUTABLE = "/home/airflow/.local/bin/dbt"
+
+# --- CONFIGURAÇÃO DO PERFIL (Conexão Segura) ---
+profile_config = ProfileConfig(
+    profile_name="meka_dw_postgres",
+    target_name="dev",
+    profile_mapping=PostgresUserPasswordProfileMapping(
+        conn_id="mekadw_airflow",
+        profile_args={"schema": "mart", "dbname": "postgres"}
+    ),
+)
+
+# --- EXECUÇÃO ---
+execution_config = ExecutionConfig(dbt_executable_path=DBT_EXECUTABLE)
+
+
+with DAG(
+    dag_id="dbt_cronogramas_int",
+    start_date=datetime(2024, 1, 1),
+    schedule="0 3 * * *",
+    catchup=False,
+    tags=["dbt", "cronogramas", "int"],
+    params={
+        "modo_debug": Param(False, type="boolean", description="Ativar modo debug? (Limita registros)"),
+        "full_refresh": Param(False, type="boolean", description="Forçar full refresh das tabelas?"),
+        "num_threads": Param(4, type="integer", description="Número de threads para a execução do dbt."),
+    }
+) as dag:
+
+    project_config = ProjectConfig(
+        dbt_project_path=DBT_PROJECT_PATH,
+    )
+
+    render_config = RenderConfig(select=["atividades_int", "banco_int", "entregas_int"])
+
+    transformacao = DbtTaskGroup(
+        group_id="dbt_run_cronogramas_int",
+        project_config=project_config,
+        profile_config=profile_config,
+        execution_config=execution_config,
+        render_config=render_config,
+        operator_args={
+            "vars": {
+                "stg_schema": "stg_cronogramas",
+                "modo_debug": "{{ params.modo_debug }}",
+            },
+            "select": "atividades_int banco_int entregas_int",
+            "threads": "{{ params.num_threads }}",
+            "full_refresh": "{{ params.full_refresh }}",
+            "args": "--fail-fast",
+        }
+    )
